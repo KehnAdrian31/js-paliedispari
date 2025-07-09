8a6c990785b39b51eed15b37aded425d4a85cb6e
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-09 15:16:42 +0200

    [add] funzione e variabili

---
+++ Palidroma/js/script.js
@@ -0,0 +1,6 @@
+//funzione per verifare se la parola è palindroma
+function isPalindroma(string){
+  //definizione variabili
+  const reversed = string.split("").reversed().join('').toLowerCase();
+
+}
