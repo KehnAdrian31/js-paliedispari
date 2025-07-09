d7f35e58c5e7c39a89c076ba04ccfb22f5163d59
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-09 15:22:49 +0200

    [add] verificazione di parola

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -3,4 +3,14 @@ function isPalindroma(string){
   //definizione variabili
   const reversed = string.split("").reversed().join('').toLowerCase();
 
+
+  //controllo se la parola inserita è uguale al suo inverso
+  if(string === reversed){
+    return true;
+  }
+    // se non è uguale
+  else{
+    return false;
+  }
 }
+
