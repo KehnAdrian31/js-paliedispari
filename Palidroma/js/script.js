89436a291550f768bac4ec86851970667d93589b
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-09 16:29:55 +0200

    [add] richiamo la funzione

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,7 +1,7 @@
 //funzione per verifare se la parola è palindroma
 function isPalindroma(string){
   //definizione variabili
-  const reversed = string.split("").reversed().join('').toLowerCase();
+  const reversed = string.split("").reverse().join('').toLowerCase();
 
 
   //controllo se la parola inserita è uguale al suo inverso
@@ -14,3 +14,14 @@ function isPalindroma(string){
   }
 }
 
+// definizione delle variabili
+const word = prompt('Inserisci la parola');
+
+
+//chiamo la funzione
+if(isPalindroma(word) === true){
+  console.log(`La parola ${word} è palindroma`)
+}
+else{
+  console.log(`La parola ${word} non è palindroma`)
+}
