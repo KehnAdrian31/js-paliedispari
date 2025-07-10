874ed686d4cf386360d9a0a52106490f78c0898d
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-10 07:26:56 +0200

    definizione della funzione

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -3,6 +3,16 @@ function randomNumber(){
   return Math.floor(Math.random() * 5) +1;
 }
 
+//definizione della funzione che stabilisce se la somma dei numeri è pari o dispari
+function evenOrOdd(sum){
+  if(sum % 2 === 0){
+    return 'pari'
+  }
+  else{
+    return 'dispari'
+  }
+}
+
 //definizione variabili
 const choice = prompt('Inserisci pari o dispari');
 const userNumber = parseInt(prompt('Inserisci in numero tra 1 e 5'));
